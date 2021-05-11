3bb7760efbce3b02cec1ae42bcbb4a90abf96779
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-11 15:44:50 +0200

    Snack 3

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-/*
-Snack 2
-Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate inizialmente a 0.
-Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
-Infine creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-
-Snack 3
-Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). 
-La funzione restituirà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
-*/
-
 //======================SNACK 1==============================
 //-----------------------ARRAY-------------------------------
 var bicycles = [
@@ -59,8 +47,6 @@ for (var i = 0; i < bicycles.length; i++) {
 // Stampa su HTML la bicicletta con il peso minore
 lessWeightBike.innerHTML = "La bici dal nome " + lightBike.name + " pesa " + lightBike.weight + " ed è la più leggera.";
 //=====================/SNACK 1==============================
-
-
 
 
 //======================SNACK 2==============================
@@ -128,23 +114,103 @@ for (i = 0; i < footballTeams.length; i++) {
 //=====================/SNACK 2==============================
 
 
-
-
 //======================SNACK 3==============================
+//-----------------------FUNZIONI----------------------------
+function valuesIncludedInTheArrayRange(arr, a, b) {
+    var newArr = [];
+    
+    for (i = (a - 1); i <= (b - 1); i++) {
+        newArr.push(arr[i]);
+    }
+
+    return newArr;
+}
+//----------------------/FUNZIONI----------------------------
 
 //-----------------------ARRAY-------------------------------
-
+var array = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J"
+]
+var newArray = [];
 //----------------------/ARRAY-------------------------------
 
-//-----------------------OGGETTI-----------------------------
-
-//----------------------/OGGETTI-----------------------------
-
 //----------------------VARIABILI----------------------------
-
+var click = false;
+var min;
+var max;
+var messageMin = "Inserisci il numero corrispondente al primo elemento da comprendere nel nuovo Array";
+var messageMax = "Inserisci il numero corrispondente all'ultimo elemento da comprendere nel nuovo Array";
+var arrayList = document.getElementById("array-list");
+var newArrayList = document.getElementById("new-array-list");
+var calculator = document.getElementById("calculator");
+var reset = document.getElementById("reset");
 //---------------------/VARIABILI----------------------------
 
+for (var j = 0; j < array.length; j++) {
+    arrayList.innerHTML += "<li>" + array[j] + "</li>";
+}
 
+// Pulsante calcolatore
+calculator.addEventListener("click", function(){
 
+    // Se il pulsante calcolatore è inattivo
+    if (!click) {
+
+        // Chiedi all'utente i due estremi che serviranno per il nuovo array
+        do {
+            min = parseInt(prompt(messageMin));
+            if (isNaN(min) || min.length == 0) {
+                messageMin = "Errore! Inserisci il numero corrispondente al primo elemento da comprendere nel nuovo Array"
+            }
+        } while (isNaN(min) || min.length == 0)
+        do {
+            max = parseInt(prompt(messageMax));
+            if (isNaN(max) || max.length == 0) {
+                messageMax = "Errore! Inserisci il numero corrispondente all'ultimo elemento da comprendere nel nuovo Array"
+            }
+        } while (isNaN(max) || min.length == 0)
+
+        // Genera il nuovo array utilizzando la funzione creata
+        newArray = valuesIncludedInTheArrayRange(array, min, max)
+
+        // Stampa su HTML il nuovo array
+        for (var k = 0; k < newArray.length; k++) {
+            newArrayList.innerHTML += "<li>" + newArray[k] + "</li>";
+        }
+
+        // Attiva il pulsante calcolatore
+        click = true;
+
+    }
+
+});
+
+// Pulsante reset
+reset.addEventListener("click", function(){
+
+    // Se il pulsante calcolatore è attivo
+    if (click) {
+
+        // Svuota il nuovo array
+        newArray = [];
+
+        // Svuota la nuova lista su HTML
+        newArrayList.innerHTML = "";
+
+        // Disattiva il pulsante calcolatore
+        click = false;
+
+    }
+
+});
 //=====================/SNACK 3==============================
 
